856a64d2e4be213fff877d1a7ae1da7fb9a402d4
florinen <florinen>
2019-07-18 11:55:57 -0500

    updated file with email notif

--- jenkins-lts/setup.groovy
+++ jenkins-lts/setup.groovy
@@ -9,6 +9,7 @@ import java.lang.reflect.*
 
 def env = System.getenv()
 
+// Create user 
 def jenkins = Jenkins.getInstance()
 if(!(jenkins.getSecurityRealm() instanceof HudsonPrivateSecurityRealm))
     jenkins.setSecurityRealm(new HudsonPrivateSecurityRealm(false))
@@ -19,7 +20,26 @@ if(!(jenkins.getAuthorizationStrategy() instanceof GlobalMatrixAuthorizationStra
 def user = jenkins.getSecurityRealm().createAccount(env.JENKINS_USER, env.JENKINS_PASS)
 user.save()
 jenkins.getAuthorizationStrategy().add(Jenkins.ADMINISTER, env.JENKINS_USER)
+
+// Set access control for builds
 Jenkins.instance.getInjector().getInstance(AdminWhitelistRule.class)
 .setMasterKillSwitch(false)
-
+// Save Jenkins
 jenkins.save()
+
+// Set URL and email
+// parameters
+
+def jenkinsParameters = [
+  email:  'Jenkins Admin <admin@jenkins.com>',
+//url:    'https://ci.jenkins.com:8083/'
+]
+// get Jenkins location configuration
+
+def jenkinsLocationConfiguration = JenkinsLocationConfiguration.get()
+//jenkinsLocationConfiguration.setUrl(jenkinsParameters.url)
+jenkinsLocationConfiguration.setAdminAddress(jenkinsParameters.email)
+// Save jenkinsLocationConfiguration
+jenkinsLocationConfiguration.save()
+
+
